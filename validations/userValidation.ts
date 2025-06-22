1216d7da510cec0f77ebf7ec633e7395180075bb
ShayMashiah <shay2908@gmail.com>
2025-06-22 16:06:02 +0300

    feat: add userPokemon validation.

--- validations/userValidation.ts
+++ validations/userValidation.ts
@@ -2,6 +2,6 @@ import Joi from 'joi';
 import { pokemonSchema } from './pokemonsValidation';
 
 export const userSchema = Joi.object({
-  id: Joi.number().integer().optional(), 
+  id: Joi.number().integer(), 
   pokemons: Joi.array().items(pokemonSchema).optional(),
 });
