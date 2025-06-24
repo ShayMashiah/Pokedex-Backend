916cc65aa626bd79010db4fc2bb21effc0751b51
ShayMashiah <shay2908@gmail.com>
2025-06-24 14:04:12 +0300

    feat: Create an API tests that checks User creation and invalid User creation

--- tests/userTests.test.ts
+++ tests/userTests.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import app from '../app';
 import prisma from '../lib/prisma';
 import { main as seedDatabase } from '../data/seed';
+import { number } from 'joi';
 
 describe('Users API Integration Test', () => {
   beforeAll(async () => {
@@ -11,5 +12,23 @@ describe('Users API Integration Test', () => {
   afterAll(async () => {
     await prisma.$disconnect();
   });
+
+    it('should create a new user', async () => {
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send({})
+      .set('Accept', 'application/json');
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty('id');
+    });
+
+    it('should create a new user with invalid data', async () => {
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send({ id: 'invalid' })
+      .set('Accept', 'application/json');
+    expect(res.statusCode).toBe(400);
+    expect(res.body.message).toBe('Wrong values inserted');
+    });
 
 });
