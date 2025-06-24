d203c79a78f156913a801c420462922ba5ef385b
ShayMashiah <shay2908@gmail.com>
2025-06-24 16:48:12 +0300

    feat: import cors to able connetion to frontend.

--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import routes from './routes/index';
-
+import cors from 'cors';
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:5173', 
+}));
 app.use(express.json());
 app.use('/api/v1', routes); 
 
