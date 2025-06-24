58af1d767795a47a7d132e6dd847807edcdadccd
ShayMashiah <shay2908@gmail.com>
2025-06-24 12:47:03 +0300

    save changes

--- tests/pokemonTests.test.ts
+++ tests/pokemonTests.test.ts
@@ -13,4 +13,12 @@ describe('Pokemons API Integration Test', () => {
     await prisma.$disconnect();
   });
 
+  describe('should get all pokemons', async () => {
+    const res = await request(app).get('/api/v1/pokemons');
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body.length).toBe(809);
+  });
+
 });
