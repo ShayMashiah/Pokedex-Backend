5fe7eb7b27bd39b7e2c9872d408e11cca6ed76bb
ShayMashiah <shay2908@gmail.com>
2025-06-26 14:45:53 +0300

    feat: add pagination and search,sortby and order route, service, controller, and tests to userPokemon.

--- controllers/userPokemonController.ts
+++ controllers/userPokemonController.ts
@@ -22,19 +22,42 @@ async function addNewPokemonToMyPokemons(req: Request, res: Response, next: Next
   }
 }
 
-async function getAllPokemonsByUserId(req: Request, res: Response, next: NextFunction) {
-    const userId = +req.params.userId;
-    const search = req.query.search as string | undefined;
-    try {
-        const pokemons = await userPokemonService.getAllPokemonsByUserId(userId, search);
-        res.status(200).json(pokemons);
-    } catch (error) {
-        if (error instanceof NotFoundError) {
-            res.status(404).json({ message: error.message });
-            return;
-        }
-        console.error('Error in getAllPokemonsByUserId:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
+export async function getAllPokemonsByUserId(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const userId = +req.params.userId;
+  const search = req.query.search as string | undefined;
+  const sortBy = (req.query.sortBy as string) || "id";
+  const order = (req.query.order as "asc" | "desc") || "asc";
+  const limit = req.query.limit ? +req.query.limit : 10;
+  const page = req.query.page ? +req.query.page : 1;
+
+  try {
+      const { data, totalCount } = await userPokemonService.getAllPokemonsByUserId(
+        userId,
+        sortBy,
+        order,
+        search,
+        limit,
+        page
+      );
+      const totalPages = Math.ceil(totalCount / limit);
+
+      res.json({
+        data,
+        totalCount,
+        totalPages,
+        currentPage: page,
+      });
+  } catch (error) {
+      if (error instanceof NotFoundError) {
+        res.status(404).json({ message: error.message });
+        return;
+      }
+      console.error("Error in getAllPokemonsByUserId:", error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
 }
 
